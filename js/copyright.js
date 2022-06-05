878925e8b5a9fbe2c21c637dd1b9cf881e687367
Mavis Javier <89547162+mavis-javier@users.noreply.github.com>
2022-06-05 13:27:31 -0500

    Created Vending Machine Object and footer portion
    Created Vending Machine Class which will be used to store information about each vending machine in JS and included a footer containing copyrights.

---
+++ js/copyright.js
@@ -0,0 +1,15 @@
+/* This program updates the year of the Copyright portion of website 
+* Created by Mavis Javier
+* July 5, 2022
+*/
+
+// get current date
+var current = new Date();
+// get current year through the year of current date
+var currentYear = current.getFullYear();
+
+// use el variable to create an object under footer id
+var el = document.getElementById("footer");
+// write the copyright text into footer's HTML section
+el.innerHTML = "<p>Copyright &copy;" + currentYear + " " + document.title + 
+                ". All Rights Reserved." +"</p>";
