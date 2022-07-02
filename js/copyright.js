3bc65f4ec9f92376ca31f52e815b701382b5e2d7
Mavis Javier <89547162+mavis-javier@users.noreply.github.com>
2022-07-02 15:38:21 -0500

    Added a Contact Us Page and bug fixes
    Created a "Contact Us" page and bug fixes on webpages and copyrights

--- js/copyright.js
+++ js/copyright.js
@@ -11,5 +11,5 @@ var currentYear = current.getFullYear();
 // use el variable to create an object under footer id
 var el = document.getElementById("footer");
 // write the copyright text into footer's HTML section
-el.innerHTML = "<p>Copyright &copy;" + currentYear + " " + document.title + 
-                ". All Rights Reserved." +"</p>";
+el.innerHTML = "<p>Copyright &copy;" + currentYear + " " + 
+"DEAJ Investments LLC. All Rights Reserved." +"</p>";
